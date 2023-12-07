d3a577d8dbb6d887c26afcabbe45348892a852de
Laszlo Szijarto <szillas92@gmail.com>
2023-12-07 10:10:29 +0100

    Blackjack is working.

--- frontend/src/components/Blackjack.jsx
+++ frontend/src/components/Blackjack.jsx
@@ -1,39 +1,285 @@
 import { useState, useEffect } from "react"
 import DealCards from "./blackjack/DealCards";
+import PlayerRound from "./blackjack/PlayerRound";
+import DealerRound from "./blackjack/DealerRound";
+import GameEndModal from "./blackjack/GameEndModal";
+import './blackjack.css'
 
 export default function Blackjack() {
     const [gameStage, SetGameStage] = useState('start');
     const [betAmount, SetBetAmount] = useState(null);
+    const [deckId, setDeckId] = useState(null);
+    const [playerCards, setPlayerCards] = useState([]);
+    const [dealerCards, setDealerCards] = useState([]);
+    const [blackjackState, setBlackjackState] = useState(null);
+    const [dealerCardValues, setDealerCardValues] = useState(null);
+    const [playerCardValues, setPlayerCardValues] = useState(null);
+    const [showModal, setShowModal] = useState(false);
 
-    function setStage(stage) {
-        SetGameStage(stage)
+    function handleSurrender() {
+        console.log(123)
+        setBlackjackState('surrender')
+        setShowModal(true)
     }
 
-    function handlePlaceBet(){
-        const inputAmount = document.getElementById('user-bet').value;
-        if(inputAmount){
-            SetGameStage('deal');
-            SetBetAmount(inputAmount)
+    function handleFirstDealEnded(deck, dealtCards, blackjack){
+        console.log(deck)
+        console.log(dealtCards)
+        console.log(blackjack)
+        setDeckId(deck)
+        setPlayerCards([dealtCards.cards[0], dealtCards.cards[2]]);
+        setDealerCards([dealtCards.cards[1], dealtCards.cards[3]]);
+        //blackjack === 'continue' ? SetGameStage('player-round') : setBlackjackState(blackjack);
+        if (blackjack === 'continue') {
+            SetGameStage('player-round');
+          } else {
+            setTimeout(() => {
+              setBlackjackState(blackjack);
+              setShowModal(true);
+            }, 2000);
+          }
+    }
+
+    /*function handlePlayerRound( playerCardValue){
+        if(playerCardValue === 21 ){
+            setBlackjackState('player')
+        }
+        if(playerCardValue > 21){
+            setBlackjackState('dealer-normal')
+        }
+    }*/
+
+    const handlePlayerRound = (playerCardValue) => {
+        handlePlayerRoundLogic(playerCardValue);
+      
+        // Show modal after 2500 milliseconds
+        setTimeout(() => {
+          setShowModal(true);
+        }, 2500);
+      };
+
+    const handlePlayerRoundLogic = (playerCardValue) => {
+        setPlayerCardValues(playerCardValue);
+        if (playerCardValue === 21) {
+          setBlackjackState('player-blackjack');
+        }
+        if (playerCardValue > 21) {
+          setBlackjackState('dealer-normal');
+        }
+      };
+
+
+
+
+    function playerStand(newCards, cardValues){
+        if(newCards.length === 0 ){
+            setDealerCardValues(dealerInitialValue())
+            setPlayerCardValues(cardValues)
+            console.log('neww round')
+            SetGameStage('dealer-round')
+        } else {
+            const cards = [...playerCards, ...newCards]
+            setPlayerCards(cards)
+            setDealerCardValues(dealerInitialValue())
+            setPlayerCardValues(cardValues)
+            console.log('neww round')
+            SetGameStage('dealer-round')
         }
     }
 
+    const dealerInitialValue = () => {
+        let dealerCardValues;
+        const initialCardValues = [dealerCards[0], dealerCards[1]];
+        const hasTwoAces = initialCardValues.filter(card => card.value === 'ACE');
+        const notAces = initialCardValues.filter(card => card.value !== 'ACE');
+        const valueOfNotAces = notAces.map(card => {
+            if(card.value === "JACK" || card.value === "QUEEN" || card.value === "KING"){
+                return 10;
+            } else {
+                return parseInt(card.value)
+            }
+        })
+        let sumOfNotAces;
+        if(valueOfNotAces.length >= 1){
+            sumOfNotAces = valueOfNotAces.reduce((total, number) => total + number);
+        } else {
+            sumOfNotAces = 0;
+        }
+
+        if(hasTwoAces.length === 2){
+            dealerCardValues = 12;
+        } 
+        if (hasTwoAces.length === 0){
+            dealerCardValues = sumOfNotAces;
+        } 
+        if((hasTwoAces.length === 1)) {
+            sumOfNotAces + 11 >= 17 ? dealerCardValues = sumOfNotAces + 11 : dealerCardValues = sumOfNotAces + 1
+        }
+        return dealerCardValues;
+    }
+
+    /*const endOfDeals = (cards = dealerCards, cardValues = dealerCardValues) => {
+        console.log(cards);
+        console.log(cardValues)
+        setDealerCardValues(cardValues)
+
+        if(cardValues=== 21){
+            setBlackjackState('dealer-bj')
+        } else if(cardValues > 21){
+            setBlackjackState('dealer-gameover')
+        } else if(cardValues > playerCardValues){
+            setBlackjackState('player-gameover')
+        } else if(cardValues < playerCardValues){
+            setBlackjackState('player-closer')
+        } else if(cardValues === playerCardValues){
+            setBlackjackState('draw-normal')
+        } else {
+            setBlackjackState('dealer-gameover')
+        }
+        setTimeout(() => {
+            setShowModal(true);
+          }, 2500);
+    }*/
+
+    const endOfDeals = (cards = dealerCards, cardValues = dealerCardValues) => {
+        console.log(cards);
+        console.log(cardValues);
+        
+        handleEndOfDeals(cardValues);
+      
+        // Show modal after 2500 milliseconds
+        setTimeout(() => {
+          setShowModal(true);
+        }, 2500);
+      };
+
+    const handleEndOfDeals = (cardValues) => {
+        setDealerCardValues(cardValues);
+      
+        // Logic to check blackjackState based on updated values
+        if (cardValues === 21) {
+          setBlackjackState('dealer-blackjack');
+        } else if (cardValues > 21) {
+          setBlackjackState('dealer-gameover');
+        } else if (cardValues > playerCardValues) {
+          setBlackjackState('player-gameover');
+        } else if (cardValues < playerCardValues) {
+          setBlackjackState('player-closer');
+        } else if (cardValues === playerCardValues) {
+          setBlackjackState('draw-normal');
+        } else {
+          setBlackjackState('dealer-gameover');
+        }
+      };
+
+      function handleStartNewGame(){
+        SetBetAmount(null);
+        setDeckId(null);
+        setPlayerCards([])
+        setDealerCards([])
+        setBlackjackState(null)
+        setDealerCardValues(null)
+        setPlayerCardValues(null)
+        setShowModal(null)
+        SetGameStage('placeBet')
+      }
+
+      
 
     return (
-        <div>
+        <>
+        <div className="bl-starter">
             {gameStage === 'start' && (
-                <>
-                    <p>Press start if you are ready to play Blackjack!</p>
-                    <button onClick={() => setStage('placeBet')}>Start Blackjack!</button>
-                </>
+                <div className="middle">
+                    <h2>Press start if you are ready to play Blackjack!</h2>
+                    <button onClick={() => SetGameStage('placeBet')}>Start Blackjack!</button>
+                </div>
             )}
             {gameStage === 'placeBet' && (
                 <>
-                    <p>Place your bet to start the game!</p>
-                    <input id='user-bet' type='number' placeholder='0'></input>
-                    <button onClick={handlePlaceBet}>Place bet!</button>
+                    <h2>Place your bet to start the game!</h2>
+                    <input id='user-bet' type='number' placeholder='0' min='0'value={betAmount} onChange={(e) => SetBetAmount(e.target.value)}></input>
+                    <button onClick={() => SetGameStage('deal')}>Place bet!</button>
                 </>
             )}
-            {gameStage === 'deal' && <DealCards bet={betAmount}/>}
+            {gameStage === 'deal' && <DealCards bet={betAmount} handleFirstDealEnded={handleFirstDealEnded}/>}
+            {gameStage === 'player-round' && <PlayerRound deckId={deckId} playerCards={playerCards} dealerCards={dealerCards} 
+            handlePlayerRound={handlePlayerRound} playerStand={playerStand} handleSurrender={handleSurrender}/>}
+            {gameStage === 'dealer-round' && <DealerRound deckId={deckId} playerCards={playerCards} dealerCards={dealerCards} 
+            dealerCardValues={dealerCardValues} playerCardValues={playerCardValues} gameOver={endOfDeals}/>}
+            {/*blackjackState === "player-round" && <div>Continue</div>*/}
+            {/*blackjackState === "draw-blackjack" && <div>You have a Blackjack! Unfortunately the house also has a blackjack. You get back your bet, which is {betAmount}</div>}
+            {blackjackState === "draw-normal" && <div>Cards values are the same for you and the dealer. You get back your bet, which is {betAmount}</div>}
+            {blackjackState === "player" && <div>You won and doubled your bet! You get {betAmount * 2} back from the house! </div>}
+            {blackjackState === "dealer-blackjack" && <div>The house has a blackjack, but you do not. You have lost your bet, {betAmount} </div>}
+            {blackjackState === "dealer-normal" && <div>Your cards value is more than 21. You have lost your bet, {betAmount} </div>}
+            {blackjackState === "dealer-gameover" && <div>The dealers cards value is more than 21. You won and doubled your bet! You get {betAmount * 2}! </div>}
+            {blackjackState === "player-gameover" && <div>The dealers cards values are closer to 21. You have lost your bet, {betAmount} </div>}
+            {blackjackState === "player-closer" && <div>Your cards values are closer to 21. You won and doubled your bet! You get {betAmount * 2}! </div>*/}
         </div>
+        {/*Modal*/}
+        {showModal && <GameEndModal blackjackState={blackjackState} betAmount={betAmount} playerCardValues={playerCardValues}
+        dealerCardValues={dealerCardValues} handleStartNewGame={handleStartNewGame}/>}
+        {/*showModal && (
+            <div className="confirmation-overlay">
+                <div className="confirmation-modal">
+                    <div className="end-modal">            
+                        {blackjackState === "player-blackjack" && (
+                            <>
+                                <h5>You have a BLACKJACK!.</h5>
+                                <h5>You doubled your bet, you get {betAmount} Forint! </h5>
+                            </>
+                        )}
+                        {blackjackState === "draw-normal" && (
+                            <>
+                                <h5>PUSH! You and the dealer also have {playerCardValues}.</h5>
+                                <h5>You get back your bet, {betAmount} Forint! </h5>
+                            </>
+                        )}
+                        {blackjackState === "dealer-normal" && (
+                            <>
+                                <h5>BUST! You have {playerCardValues}.</h5>
+                                <h5>You have lost your bet, {betAmount} Forint! </h5>
+                            </>
+                        )}
+                        {blackjackState === "draw-blackjack" && (
+                            <>
+                                <h5>PUSH! You and the dealer also have a blackjack!</h5>
+                                <h5>You get back your bet, {betAmount} Forint. </h5>
+                            </>
+                        )}
+                        {blackjackState === "dealer-blackjack" && (
+                            <>
+                                <h5>Dealer has a blackjack!</h5>
+                                <h5>You have lost your bet, {betAmount} Forint. </h5>
+                            </>
+                        )}
+                        {blackjackState === "dealer-gameover" && (
+                            <>
+                                <h5>Dealer BUST! Dealer has {dealerCardValues}.</h5>
+                                <h5>You won and doubled your bet! You get {betAmount * 2} Forint! </h5>
+                            </>
+                        )}
+                        {blackjackState === "player-gameover" && (
+                            <>
+                                <h5>Dealer has {dealerCardValues}, you have {playerCardValues}.</h5>
+                                <h5>Dealer is closer to 21. You have lost your bet, {betAmount} Forint. </h5>
+                            </>
+                        )}
+                        {blackjackState === "player-closer" && (
+                            <>
+                                <h5>Dealer has {dealerCardValues}, you have {playerCardValues}.</h5>
+                                <h5>Your cards are closer to 21. You won and doubled your bet! You get {betAmount * 2} Forint! </h5>
+                            </>
+                        )}
+                    </div>
+                    <>
+                        <button onClick={handleStartNewGame}>Start new game</button>
+                        <button>Back to main page</button>
+                    </>
+                </div>
+            </div>
+                        )*/}
+        </>
     )
 }
--- frontend/src/components/blackjack/DealCards.jsx
+++ frontend/src/components/blackjack/DealCards.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react"
-import Dealt4Cards from "./Dealt4Cards";
 
-export default function DealCards({bet}) {
+export default function DealCards({bet, handleFirstDealEnded}) {
     const [dealId, setDealId] = useState(null)
+    const [dealtCards, setDealtCards] = useState(null)
+    const [blackjackState, setBlackjackState] = useState(null)
 
 
     useEffect(() => {
@@ -15,11 +16,84 @@ export default function DealCards({bet}) {
         .catch((error) => console.log(error));
     }, []);
 
+    useEffect(() => {
+        console.log(dealId)
+        if(dealId){
+            fetch(`https://www.deckofcardsapi.com/api/deck/${dealId}/draw/?count=4`)
+            .then((response) => response.json())
+            .then((cards) => {
+            console.log(cards);
+            getBlackJack(cards);
+            })
+            .catch((error) => console.log(error));
+        }
+    }, [dealId]);
+
+
+    function getCardValues(card1, card2){
+        console.log(card1, card2)
+        const sumOfCardValues = getLetterLapValue(card1) + getLetterLapValue(card2);
+        if(sumOfCardValues === 22){
+            return 12;
+        } else {
+            return sumOfCardValues;
+        }
+
+    }
+
+    function getLetterLapValue (card){
+        const value = (card === "JACK" ? 10 : card === "QUEEN" ? 10 : card === "KING" ? 10 : card === "ACE" ? 11 : parseInt(card))
+        return value
+    }
+
+    function getBlackJack (cards) {
+        const playerCardValues = getCardValues(cards.cards[0].value, cards.cards[2].value);
+        const dealerCardValues = getCardValues(cards.cards[1].value, cards.cards[3].value);
+
+        const blackjack = (playerCardValues === 21 && dealerCardValues === 21) ? "draw-blackjack" :
+        playerCardValues === 21 ? "player-blackjack" : dealerCardValues === 21 ? "dealer-blackjack" : "continue";
+        console.log(dealtCards)
+        setDealtCards(cards);
+        setBlackjackState(blackjack)
+        handleFirstDealEnded(dealId, cards, blackjack);
+        return blackjack
+    }
 
     return (
         <>
-           { !dealId && <p>Deal Cards</p>}
-           { dealId && <Dealt4Cards deckid={dealId} bet={bet}/>}
+           { !dealId && <h2>Dealing cards</h2>}
+           {/* dealId && <Dealt4Cards deckid={dealId} bet={bet}/>*/}
+           { dealtCards &&(
+                <>
+                    <div className="bj-dealer-upper">
+                        <h4>Dealers cards are:</h4>
+                        {/*<div>{`${dealtCards.cards[1].value} of ${dealtCards.cards[1].suit}`}</div>
+                        <div>{`${dealtCards.cards[3].value} of ${dealtCards.cards[3].suit}`}</div>
+                        <div>CardValues: {getCardValues(dealtCards.cards[1].value, dealtCards.cards[3].value)}</div>*/}
+                        <div className="bj-dealer">
+                            <div className="bj-card"><img src={dealtCards.cards[1].images.png} alt={`${dealtCards.cards[1].value} of ${dealtCards.cards[1].suit}`} /></div>
+                            {(blackjackState === 'draw-blackjack' || blackjackState === 'player-blackjack' || blackjackState === 'dealer-blackjack') && (
+                                <div className="bj-card"><img src={dealtCards.cards[3].images.png} alt={`${dealtCards.cards[3].value} of ${dealtCards.cards[3].suit}`} /></div>
+                            )}
+                            {!blackjackState && <div className="bj-card"><img src={'https://www.deckofcardsapi.com/static/img/back.png'} alt={`Dealer's 2nd card`} /></div>}
+                        </div>
+                    </div>
+                    <div className="bj-separator"></div>
+                    <div className="bj-player-upper">
+                        <h4>Your cards are:</h4>
+                        {/*<div>CardValues: {getCardValues(dealtCards.cards[0].value, dealtCards.cards[2].value)}</div>
+                        <div >{`${dealtCards.cards[0].value} of ${dealtCards.cards[0].suit}`}</div>
+                        <div >{`${dealtCards.cards[2].value} of ${dealtCards.cards[2].suit}`}</div>*/}
+                        <div className="bj-player">
+                            <div className="bj-card"><img src={dealtCards.cards[0].images.png} alt={`${dealtCards.cards[0].value} of ${dealtCards.cards[0].suit}`} /></div>
+                            <div className="bj-card"><img src={dealtCards.cards[2].images.png} alt={`${dealtCards.cards[2].value} of ${dealtCards.cards[2].suit}`} /></div>
+                        </div>
+                    </div>
+                </>
+           )}
+            {/*{blackjackState === "continue" && <div>Continue</div>}
+           {blackjackState === "draw" && <div>You have a Blackjack! Unfortunately the house also has a blackjack. You get back your bet, which is {bet}</div>}
+           {blackjackState === "player" && <div>You won and doubled your bet! You get {bet * 2} back from the house! </div>}*/}
         </>
     )
 }
